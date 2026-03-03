f24797c3a8097699c342cf80afd1b730da3eb854
Milan Mitic <milanmitic@Milans-MacBook-Pro.local>
2026-03-03 15:43:57 +0100

    Disabled orderbook_history cleanup

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('orderbook:clean-history')->hourly();
 Schedule::command('trades:aggregate')->everyMinute();
 Schedule::command('binance:fetch-open-interest')->everyThirtySeconds();
 Schedule::command('binance:clean-futures-history')->hourly();
